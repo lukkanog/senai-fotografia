24e53aa5c9c07cf258679c51dc700b3dd6af4ca9
lukkanog <lukanog@hotmail.com>
2021-02-12 19:28:14 -0300

    vercel deploy attempt fix

--- src/components/Gallery/index.js
+++ src/components/Gallery/index.js
@@ -5,24 +5,24 @@ import Image from 'next/image';
 import ImageContainer from "./Image";
 import GalleryGrid from "./styles";
 
-import img01 from "../../assets/img/gallery/img01.JPG";
-import img02 from "../../assets/img/gallery/img02.JPG";
-import img03 from "../../assets/img/gallery/img03.JPG";
-import img04 from "../../assets/img/gallery/img04.JPG";
-import img05 from "../../assets/img/gallery/img05.JPG";
-// import img06 from "../../assets/img/gallery/img06.JPG";
-import img07 from "../../assets/img/gallery/img07.jpg";
-import img08 from "../../assets/img/gallery/img08.JPG";
-import img09 from "../../assets/img/gallery/img09.JPG";
-import img10 from "../../assets/img/gallery/img10.JPG";
-import img11 from "../../assets/img/gallery/img11.JPG";
-import img12 from "../../assets/img/gallery/img12.JPG";
-import img13 from "../../assets/img/gallery/img13.JPG";
-import img14 from "../../assets/img/gallery/img14.JPG";
-import img15 from "../../assets/img/gallery/img15.JPG";
-import img16 from "../../assets/img/gallery/img16.JPG";
-import img17 from "../../assets/img/gallery/img17.JPG";
-import img18 from "../../assets/img/gallery/img18.JPG";
+import Img01 from "../../assets/img/gallery/img01.JPG";
+import Img02 from "../../assets/img/gallery/img02.JPG";
+import Img03 from "../../assets/img/gallery/img03.JPG";
+import Img04 from "../../assets/img/gallery/img04.JPG";
+import Img05 from "../../assets/img/gallery/img05.JPG";
+import Img06 from "../../assets/img/gallery/img06.JPG";
+import Img07 from "../../assets/img/gallery/img07.jpg";
+import Img08 from "../../assets/img/gallery/img08.JPG";
+import Img09 from "../../assets/img/gallery/img09.JPG";
+import Img10 from "../../assets/img/gallery/img10.JPG";
+import Img11 from "../../assets/img/gallery/img11.JPG";
+import Img12 from "../../assets/img/gallery/img12.JPG";
+import Img13 from "../../assets/img/gallery/img13.JPG";
+import Img14 from "../../assets/img/gallery/img14.JPG";
+import Img15 from "../../assets/img/gallery/img15.JPG";
+import Img16 from "../../assets/img/gallery/img16.JPG";
+import Img17 from "../../assets/img/gallery/img17.JPG";
+import Img18 from "../../assets/img/gallery/img18.JPG";
 
 
 
@@ -50,60 +50,60 @@ function Gallery() {
 
             <GalleryGrid.Row>
                 <GalleryGrid.Column>
-                    <Photo alt="" src={img01}/>
-                    <Photo alt="" src={img02}/>
+                    <Photo alt="" src={Img01}/>
+                    <Photo alt="" src={Img02}/>
                 </GalleryGrid.Column>
 
                 <GalleryGrid.Column>
-                    <Photo alt="" src={img03} />
-                    <Photo alt="" src={img04} />
+                    <Photo alt="" src={Img03} />
+                    <Photo alt="" src={Img04} />
                 </GalleryGrid.Column>
 
             </GalleryGrid.Row>
 
             <GalleryGrid.Row>
                 <GalleryGrid.Column>
-                    <Photo alt="" src={img07} />
+                    <Photo alt="" src={Img06} />
                 </GalleryGrid.Column>
 
                 <GalleryGrid.Column>
-                    <Photo alt="" src={img07} />
-                </GalleryGrid.Column>
-            </GalleryGrid.Row>
-
-            <GalleryGrid.Row>
-                <GalleryGrid.Column>
-                    <Photo alt="" src={img08} />
-                    <Photo alt="" src={img09} />
-                </GalleryGrid.Column>
-
-                <GalleryGrid.Column>
-                    <Photo alt="" src={img10}/>
-                    <Photo alt="" src={img11}/>
+                    <Photo alt="" src={Img07} />
                 </GalleryGrid.Column>
             </GalleryGrid.Row>
 
             <GalleryGrid.Row>
                 <GalleryGrid.Column>
-                    <Photo alt="" src={img12}/>
-                    <Photo alt="" src={img13}/>
+                    <Photo alt="" src={Img08} />
+                    <Photo alt="" src={Img09} />
                 </GalleryGrid.Column>
 
                 <GalleryGrid.Column>
-                    <Photo alt="" src={img14} />
-                    <Photo alt="" src={img15} />
+                    <Photo alt="" src={Img10}/>
+                    <Photo alt="" src={Img11}/>
                 </GalleryGrid.Column>
             </GalleryGrid.Row>
 
             <GalleryGrid.Row>
                 <GalleryGrid.Column>
-                    <Photo alt="" src={img16} />
-                    <Photo alt="" src={img17} />
+                    <Photo alt="" src={Img12}/>
+                    <Photo alt="" src={Img13}/>
                 </GalleryGrid.Column>
 
                 <GalleryGrid.Column>
-                    <Photo alt="" src={img18} />
-                    <Photo alt="" src={img05} />
+                    <Photo alt="" src={Img14} />
+                    <Photo alt="" src={Img15} />
+                </GalleryGrid.Column>
+            </GalleryGrid.Row>
+
+            <GalleryGrid.Row>
+                <GalleryGrid.Column>
+                    <Photo alt="" src={Img16} />
+                    <Photo alt="" src={Img17} />
+                </GalleryGrid.Column>
+
+                <GalleryGrid.Column>
+                    <Photo alt="" src={Img18} />
+                    <Photo alt="" src={Img05} />
                 </GalleryGrid.Column>
             </GalleryGrid.Row>
 
